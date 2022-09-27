8bd62433206f03b2123b0bc57424403f0ca72bb9
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-09-27 18:06:44 +0300

    variables

---
+++ 2-Variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int studentNumber = 12;
+		String mesage = "Number of student is : ";
+		System.out.println("Number of student is : "+ studentNumber);
+		System.out.println(mesage + studentNumber );
+		System.out.println(mesage + "12");
+	}
+}
