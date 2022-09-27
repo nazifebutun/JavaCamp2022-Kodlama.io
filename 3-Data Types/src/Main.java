ecb95e9ff9d252e4b35375011dacc909b0a06d21
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-09-27 18:15:25 +0300

    dataTypes

---
+++ 3-Data Types/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 128;
+		byte number2 = 127;
+		short number3 = 32767;
+		double number4 = 12.5;
+		
+		char character = 'N';
+		boolean isittrue = false;
+	}
+}
