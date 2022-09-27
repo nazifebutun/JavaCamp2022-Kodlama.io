45395d1686f89a6308b64ab003efc481b17d4172
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-09-27 18:25:39 +0300

    condition

---
+++ 4-Conditionals (if)/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 15;
+		if (number<20) {
+			System.out.println("Number is less than 20");		
+		}else if (number == 20) {
+			System.out.println("Number is 20");
+		}else {
+			System.out.println("Number is bigger than 20");
+		}		
+	}
+}
