acc464713fc4ad0c3d617a02bf6a1d1bce6571d8
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-09-29 20:17:51 +0300

    for-while-doWhile

---
+++ 6-Loops/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//for
+		for(int i=1; i<10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("For loop is done.");
+		
+		//while
+		int i=1;
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While loop is done.");
+		
+		//doWhile
+		int j=1;
+		 do {
+			 System.out.println(j);
+			 j++;
+		 }while(j<10);
+		 System.out.println("Do while loop is done.");
+	}
+}
