f70193a80025167d2ef389ed5b6050fbbb6d5d71
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-10-08 16:11:36 +0300

    finds number

---
+++ Mini Project - Finds Number/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int find = 2;
+		Boolean isThere = false;
+		
+		for (int i = 0; i < numbers.length; i++) {
+			if (find == numbers[i]) {
+				isThere = true;
+				break;
+			}	
+		}
+		if(isThere == true) {
+			System.out.println("The sought number is here.");
+		}
+		else {
+			System.out.println("The sought number is not here.");
+		}
+}
+}
