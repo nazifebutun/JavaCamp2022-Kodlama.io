8e6a776c3edc8806935c633b40407952b69466e9
nazifebutun <113862962+nazifebutun@users.noreply.github.com>
2022-10-16 14:58:49 +0300

    classes

---
+++ 13-Classes/src/CustomerManager.java
@@ -0,0 +1,12 @@
+public class CustomerManager {
+	public void add() {
+		System.out.println("Customer is added.");
+	}
+	public void remove() {
+		System.out.println("Customer is  removed.");
+	}
+	public void update() {
+		System.out.println("Customer is updated.");
+	}
+
+}
---
+++ 13-Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customermanager = new CustomerManager();
+		CustomerManager customermanager2 = new CustomerManager();
+		customermanager2.add();
+	}
+}
